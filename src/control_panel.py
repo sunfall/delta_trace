c55c1a4d0268b008d80f7a86241594d86e8a55f0
Adam Zapletal <adamzap@gmail.com>
2009-07-24 23:42:45 -0500

    Added image transforms to init of ControlPanelSkin

--- src/control_panel.py
+++ src/control_panel.py
@@ -4,7 +4,9 @@ import ConfigParser
 
 class ControlPanelError(Exception): pass
 
+
 class ControlPanelSkinError(Exception): pass
+
 
 class ControlPanel(object):
     def __init__(self, skin_name):
@@ -13,7 +15,11 @@ class ControlPanel(object):
         self.cached_skins = {}
         self.last_skin_name = self.current_skin_name
 
+        # TODO: Fix path (has to be run from src dir to work in this state)
+        self.map_path = os.path.join('..', 'data', 'control_panel.map')
+
         self.load_skin(self.current_skin_name)
+        self.build_image()
 
     def load_skin(self, skin_name):
         """
@@ -41,6 +47,10 @@ class ControlPanel(object):
 
         self.skin = self.cached_skins[skin_name]
 
+    def build_image(self):
+        map_lines = [line.strip() for line in open(self.map_path).readlines()]
+
+
 class ControlPanelSkin(object):
     def __init__(self, skin_name):
         # TODO: Fix path (has to be run from src dir to work in this state)
@@ -56,10 +66,31 @@ class ControlPanelSkin(object):
         image_files = dat_parser.items('Image Files')
         name_to_path = dict([(k, skin_dir + os.sep + v) for k,v in image_files])
 
-        self.straight_img = pyglet.image.load(name_to_path['straight'])
-        self.corner_img = pyglet.image.load(name_to_path['corner'])
-        self.plus_img = pyglet.image.load(name_to_path['plus'])
-        self.border_split_img = pyglet.image.load(name_to_path['border_split'])
+        straight_img = pyglet.image.load(name_to_path['straight'])
+        corner_img = pyglet.image.load(name_to_path['corner'])
+        plus_img = pyglet.image.load(name_to_path['plus'])
+        border_split_img = pyglet.image.load(name_to_path['border_split'])
+
+        self.images = {}
+
+        # straight
+        self.images['vertical'] = straight_img.get_texture()
+        self.images['horizontal'] = straight_img.texture.get_transform(rotate=90)
+
+        # corners
+        self.images['nw_corner'] = corner_img.get_texture()
+        self.images['ne_corner'] = corner_img.texture.get_transform(rotate=90)
+        self.images['se_corner'] = corner_img.texture.get_transform(rotate=180)
+        self.images['sw_corner'] = corner_img.texture.get_transform(rotate=270)
+
+        # border splits
+        self.images['bs_e'] = border_split_img.get_texture()
+        self.images['bs_s'] = border_split_img.texture.get_transform(rotate=90)
+        self.images['bs_w'] = border_split_img.texture.get_transform(rotate=180)
+        self.images['bs_n'] = border_split_img.texture.get_transform(rotate=270)
+
+        # plus
+        self.images['plus'] = plus_img.get_texture()
 
 if __name__ == '__main__':
     control_panel = ControlPanel('basic')
