4b5736e10c14d6c16992f51fceb2a2f4cf7cf2e3
Adam Zapletal <adamzap@gmail.com>
2009-07-26 01:34:22 -0500

    Removed image generation parts from control_panel

--- src/control_panel.py
+++ src/control_panel.py
@@ -1,6 +1,5 @@
 import os
 import pyglet
-import ConfigParser
 
 class ControlPanelError(Exception): pass
 
@@ -10,87 +9,45 @@ class ControlPanelSkinError(Exception): pass
 
 class ControlPanel(object):
     def __init__(self, skin_name):
-        self.current_skin_name = skin_name
+        self.skin_name = 'basic'
 
         self.cached_skins = {}
-        self.last_skin_name = self.current_skin_name
 
-        # TODO: Fix path (has to be run from src dir to work in this state)
-        self.map_path = os.path.join('..', 'data', 'control_panel.map')
+        self.switch_skin(self.skin_name)
 
-        self.load_skin(self.current_skin_name)
-        self.build_image()
+    def _cache_skin(self, skin_name):
+        skin_dir = os.path.join('..', 'data', 'skins', skin_name)
+        cp_image = pyglet.image.load(os.path.join(skin_dir,'control_panel.png'))
 
-    def load_skin(self, skin_name):
-        """
-        Initialize a new ControlPanelSkin object for the specified skin
-        """
-
-        loaded_skin = ControlPanelSkin(skin_name)
-
-        self.skin = loaded_skin
-        self.cached_skins[skin_name] = loaded_skin
-
-        self.current_skin_name = skin_name
+        self.cached_skins[skin_name] = cp_image
 
     def switch_skin(self, skin_name):
         """
         Switch the current skin of this control panel to the specified skin.
 
-        If the specified skin is not cached, load it and cache it.
+        If the specified skin is not cached, cache it.
         """
 
-        self.last_skin_name = self.current_skin_name
+        self.last_skin_name = self.skin_name
 
         if skin_name not in self.cached_skins.keys():
-            self.load_skin(self, skin_name)
+            self._cache_skin(skin_name)
 
-        self.skin = self.cached_skins[skin_name]
+        self.skin_name = skin_name
+        self.image = self.cached_skins[skin_name]
 
-    def build_image(self):
-        map_lines = [line.strip() for line in open(self.map_path).readlines()]
+    def draw(self):
+        self.image.blit(0, 0)
 
-
-class ControlPanelSkin(object):
-    def __init__(self, skin_name):
-        # TODO: Fix path (has to be run from src dir to work in this state)
-        skin_dir = os.path.join('..', 'data', 'control_panels', skin_name)
-
-        dat_parser = ConfigParser.SafeConfigParser()
-
-        try:
-            dat_parser.readfp(open(os.path.join(skin_dir, skin_name + '.dat')))
-        except IOError:
-            raise ControlPanelSkinError, 'skin %s was not found' % skin_name
-
-        image_files = dat_parser.items('Image Files')
-        name_to_path = dict([(k, skin_dir + os.sep + v) for k,v in image_files])
-
-        straight_img = pyglet.image.load(name_to_path['straight'])
-        corner_img = pyglet.image.load(name_to_path['corner'])
-        plus_img = pyglet.image.load(name_to_path['plus'])
-        border_split_img = pyglet.image.load(name_to_path['border_split'])
-
-        self.images = {}
-
-        # straight
-        self.images['vertical'] = straight_img.get_texture()
-        self.images['horizontal'] = straight_img.texture.get_transform(rotate=90)
-
-        # corners
-        self.images['nw_corner'] = corner_img.get_texture()
-        self.images['ne_corner'] = corner_img.texture.get_transform(rotate=90)
-        self.images['se_corner'] = corner_img.texture.get_transform(rotate=180)
-        self.images['sw_corner'] = corner_img.texture.get_transform(rotate=270)
-
-        # border splits
-        self.images['bs_e'] = border_split_img.get_texture()
-        self.images['bs_s'] = border_split_img.texture.get_transform(rotate=90)
-        self.images['bs_w'] = border_split_img.texture.get_transform(rotate=180)
-        self.images['bs_n'] = border_split_img.texture.get_transform(rotate=270)
-
-        # plus
-        self.images['plus'] = plus_img.get_texture()
 
 if __name__ == '__main__':
     control_panel = ControlPanel('basic')
+
+    window = pyglet.window.Window(256, 240)
+
+    @window.event
+    def on_draw():
+        window.clear()
+        control_panel.draw()
+
+    pyglet.app.run()
